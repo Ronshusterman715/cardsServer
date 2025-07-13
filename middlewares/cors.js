04f562b82913164102b9eddeb08cceec4468a5cd
Ronshusterman715 <ron7892123@gmail.com>
2025-07-13 16:29:12 +0300

    cors update

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,7 +3,8 @@ const cors = require('cors');
 const corsMiddleware = cors({
     origin: [
         "http://127.0.0.1:5500",
-        "http://localhost:5173"
+        "http://localhost:5173",
+        "https://card-react-app-including-own-uploaded.onrender.com"
     ]
 })
 
